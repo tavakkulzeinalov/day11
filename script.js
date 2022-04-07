382fb62199b782ffeefe781c2b3a74ccc623ab6d
Təvəkkül <tevizeynalov@gmail.com>
2022-04-07 22:59:50 +0400

    поигрался чуток

--- script.js
+++ script.js
@@ -1,5 +1,4 @@
 const btn = document.getElementById('btn');
-const square = document.getElementById('square');
 const e_btn = document.getElementById('e_btn');
 const range = document.getElementById('range');
 
@@ -13,7 +12,12 @@ btn.onclick = function () {
 range.onclick = function () {
     const w = range.value;
     const h = range.value;
+    const br = range.value;
     const circle = document.getElementById('circle');
+    const square = document.getElementById('square');
     circle.style.width = w + '%';
     circle.style.height = h + '%';
+    circle.style.borderRadius = br + '%';
+    square.style.borderRadius = br + '%';
+    circle.innerText = 'hi';
 };
