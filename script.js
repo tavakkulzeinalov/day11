ba33cd10571fcba5612ecb62036eca936d87b08b
Təvəkkül <tevizeynalov@gmail.com>
2022-04-07 16:53:32 +0400

    сделал управляемыми элементы

--- script.js
+++ script.js
@@ -1,6 +1,19 @@
-console.log('hello js');
+const btn = document.getElementById('btn');
+const square = document.getElementById('square');
+const e_btn = document.getElementById('e_btn');
+const range = document.getElementById('range');
 
+e_btn.style.display = 'none';
 
+function changeColor() {
+    const text = document.getElementById('text').value;
+    square.style.backgroundColor = text;
+}
 
-
-
+function changeCircle() {
+    const w = range.value;
+    const h = range.value;
+    const circle = document.getElementById('circle');
+    circle.style.width = w + '%';
+    circle.style.height = h + '%';
+}
